b4c9a0274b5a6afc585788d488f4c0dc76dc5fd8
itsmebenpax <benjamin.parbst95@gmail.com>
2020-02-14 10:08:36 +0100

    added components

---
+++  components/elements/menu_item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function menu_item() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++  components/pages/member_page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class member_page extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
