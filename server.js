52c569fb52aa79b944e97ac8299a9afef6c12ccf
Morten Skovgaard Jensen <mojen17@student.sdu.dk>
2020-03-26 15:32:41 +0100

    added connection to AWS and fetch user based on membership_number

--- server.js
+++ server.js
@@ -49,6 +49,26 @@ app.post('/api/user/login', async (req, res) => {
         return res.status(404).send('User not found!')}
 });
 
+app.post('/api/user/getUserID', async (req,res) => {
+    console.log("req body: ",req.body)
+    try {
+        const user = await axios.post(process.env.AWS_ENDPOINT+'getUser',req.body,{headers: {'x-api-key':process.env.AWS_KEY}});
+        console.log("user body: ",user.data)
+        return res.status(status).send(user.body)
+    } catch (error) {
+        //console.log('RES: ',res)
+        console.log("Error: ")
+        return res.send("error")
+    } 
+    
+})
+app.get('/api/user/getUser', async (req,res) => {
+    console.log(req)
+    const user = await axios.get(process.env.AWS_ENDPOINT+'getUser', {headers: {'x-api-key': process.env.AWS_KEY}})
+    console.log('User data: ',user.data.body);
+    return res.send(user.data)
+})
+
 
 app.post('/api/user/createUser', async (req, res) => {
     const user = {
@@ -57,13 +77,16 @@ app.post('/api/user/createUser', async (req, res) => {
         membership_number: req.body.membership_number,
         password_hash: req.body.password
     }
-    console.log(user);
-    try {
+    console.log("Server " ,typeof(user));
+    var userJson = JSON.stringify(user);
+    console.log("JSON user: ",typeof(userJson));
+    /*try {
         const result = await axios.post(process.env.AWS_ENDPOINT+'createUser', {headers: {'x-api-key': process.env.AWS_KEY}}, user);
         return res.send(result.status) 
     } catch (error) {
         return res.send('shit broke ', error)
-    }
+    }*/
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
