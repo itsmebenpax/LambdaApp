aa8db83227a5d297095d4ae1cc05218c8da4943d
Morten Skovgaard Jensen <mojen17@student.sdu.dk>
2020-03-31 12:23:33 +0200

    added create user function

--- server.js
+++ server.js
@@ -28,10 +28,38 @@ let usersList =[{
     password: "password"
 }]
 
+app.get('/api/user/getUser', async (req,res) => {
+    console.log(req)
+    const user = await axios.get(process.env.AWS_ENDPOINT+'getUser', {headers: {'x-api-key': process.env.AWS_KEY}})
+    console.log('User data: ',user.data.body);
+    return res.send(user.data)
+});
 
-app.get('/api/user', async (req, res) => {
-    const result = usersList.find(({ userName }) => userName === req.body.username );
-return res.send(result)});
+app.post('/api/user/getUserID', async (req,res) => {
+    try {
+        const user = await axios.post(process.env.AWS_ENDPOINT+'getUser',req.body,{headers: {'x-api-key':process.env.AWS_KEY}});
+        return res.send(user.data.body.Items[0])
+    } catch (error) {
+        return res.send("error")
+    }
+});
+
+app.post('/api/user/createUser', async (req, res) => {
+    const user = {
+        user_id: req.body.user_id,
+        email: req.body.email,
+        membership_number: req.body.membership_number,
+        password_hash: req.body.password
+    }
+    try {
+        console.log("User object: ",user)
+        const result = await axios.post(process.env.AWS_ENDPOINT+'createUser', user, {headers: {'x-api-key': process.env.AWS_KEY}});
+        console.log("result: ",result.status)
+        return res.send(result.status) 
+    } catch (error) {
+        return res.status("Error")
+    }
+});
 
 app.post('/api/user/login', async (req, res) => {
     console.log(req.body)
@@ -49,44 +77,4 @@ app.post('/api/user/login', async (req, res) => {
         return res.status(404).send('User not found!')}
 });
 
-app.post('/api/user/getUserID', async (req,res) => {
-    console.log("req body: ",req.body)
-    try {
-        const user = await axios.post(process.env.AWS_ENDPOINT+'getUser',req.body,{headers: {'x-api-key':process.env.AWS_KEY}});
-        console.log("user body: ",user.data)
-        return res.status(status).send(user.body)
-    } catch (error) {
-        //console.log('RES: ',res)
-        console.log("Error: ")
-        return res.send("error")
-    } 
-    
-})
-app.get('/api/user/getUser', async (req,res) => {
-    console.log(req)
-    const user = await axios.get(process.env.AWS_ENDPOINT+'getUser', {headers: {'x-api-key': process.env.AWS_KEY}})
-    console.log('User data: ',user.data.body);
-    return res.send(user.data)
-})
-
-
-app.post('/api/user/createUser', async (req, res) => {
-    const user = {
-        user_id: req.body.user_id,
-        email: req.body.email,
-        membership_number: req.body.membership_number,
-        password_hash: req.body.password
-    }
-    console.log("Server " ,typeof(user));
-    var userJson = JSON.stringify(user);
-    console.log("JSON user: ",typeof(userJson));
-    /*try {
-        const result = await axios.post(process.env.AWS_ENDPOINT+'createUser', {headers: {'x-api-key': process.env.AWS_KEY}}, user);
-        return res.send(result.status) 
-    } catch (error) {
-        return res.send('shit broke ', error)
-    }*/
-})
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
